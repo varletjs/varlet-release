17d2f704a7a680e3cedfd9ddec0c29ed36c693a1
a145789 <2091927351@qq.com>
2025-05-20 20:57:04 +0800

    feat(changelog): add writerOpts parameter to changelog function

--- src/changelog.ts
+++ src/changelog.ts
@@ -8,6 +8,7 @@ const { createWriteStream } = fse
 export interface ChangelogCommandOptions {
   file?: string
   releaseCount?: number
+  writerOpts?: Parameters<typeof conventionalChangelog>['4']
   preset?:
     | 'angular'
     | 'atom'
@@ -24,14 +25,21 @@ export function changelog({
   releaseCount = 0,
   file = 'CHANGELOG.md',
   preset = 'angular',
+  writerOpts,
 }: ChangelogCommandOptions = {}): Promise<void> {
   const s = createSpinner('Generating changelog').start()
 
   return new Promise((resolve) => {
-    conventionalChangelog({
-      preset,
-      releaseCount,
-    })
+    conventionalChangelog(
+      {
+        preset,
+        releaseCount,
+      },
+      undefined,
+      undefined,
+      undefined,
+      writerOpts,
+    )
       .pipe(createWriteStream(resolvePath(process.cwd(), file)))
       .on('close', () => {
         s.success({ text: 'Changelog generated success!' })
