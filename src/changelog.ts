c682b35b431becc7aa6c06e742ca550cbd88125e
clen cat <37403253+a145789@users.noreply.github.com>
2025-05-20 22:59:52 +0800

    feat(changelog): add writerOpts to customize commit type transformations (#31)

--- src/changelog.ts
+++ src/changelog.ts
@@ -18,20 +18,44 @@ export interface ChangelogCommandOptions {
     | 'express'
     | 'jquery'
     | 'jshint'
+  writerOpts?: Parameters<typeof conventionalChangelog>['4']
 }
 
 export function changelog({
   releaseCount = 0,
   file = 'CHANGELOG.md',
   preset = 'angular',
+  writerOpts = {
+    transform: (commit: any) => {
+      if (commit.type === `feat`) {
+        commit.type = `Features`
+      } else if (commit.type === `fix`) {
+        commit.type = `Bug Fixes`
+      } else if (commit.type === `perf`) {
+        commit.type = `Performance Improvements`
+      } else if (commit.type === `refactor`) {
+        commit.type = `Refactor`
+      } else {
+        return false
+      }
+
+      return commit
+    },
+  },
 }: ChangelogCommandOptions = {}): Promise<void> {
   const s = createSpinner('Generating changelog').start()
 
   return new Promise((resolve) => {
-    conventionalChangelog({
-      preset,
-      releaseCount,
-    })
+    conventionalChangelog(
+      {
+        preset,
+        releaseCount,
+      },
+      undefined,
+      undefined,
+      undefined,
+      writerOpts,
+    )
       .pipe(createWriteStream(resolvePath(process.cwd(), file)))
       .on('close', () => {
         s.success({ text: 'Changelog generated success!' })
