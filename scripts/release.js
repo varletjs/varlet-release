b235d95b55bbc1d161826fb160e3709e779994d7
a145789 <2091927351@qq.com>
2026-02-07 11:39:46 +0800

    chore(release): add build progress logs to release script

--- scripts/release.js
+++ scripts/release.js
@@ -4,6 +4,8 @@ import { release } from '../dist/index.js'
 release({
   checkRemoteVersion: true,
   task: async () => {
+    console.log('Building the project...')
     await exec('pnpm', ['run', 'build'])
+    console.log('Build completed.')
   },
 })
